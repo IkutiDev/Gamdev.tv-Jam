0af413b6040b0f1aad52ad05d8940481559090ab
Ikuti <m.j.olkiewicz@gmail.com>
2021-06-04 01:26:20 +0200

    Added mechaGnome

---
+++ Gamedev-tv-Jam/Assets/EnemyProjectile.cs
@@ -0,0 +1,35 @@
+using Gamedev.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public int damage;
+    [SerializeField] private float speed;
+    public bool goLeft;
+    // Update is called once per frame
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+    void Update()
+    {
+        if (goLeft)
+        {
+            transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + (speed * Time.deltaTime), transform.position.y, transform.position.z);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>())
+        {
+            if (other.tag != "Player") return;
+            other.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
--- Gamedev-tv-Jam/Assets/RoboGnomeController.cs
+++ Gamedev-tv-Jam/Assets/RoboGnomeController.cs
@@ -1,3 +1,4 @@
+using Gamedev.Combat;
 using Gamedev.Control;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using UnityEngine;
 public class RoboGnomeController : MonoBehaviour
 {
     PlayerController player;
-    [SerializeField] Vector3 randomPosition;
+    Vector3 randomLocalPosition;
     [SerializeField] private float speed;
     [SerializeField] private float damping;
 
@@ -15,18 +16,50 @@ public class RoboGnomeController : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        randomPosition = new Vector3(0, 0, Random.Range(0, 18));
+        int random = Random.Range(0, 18);
+        randomLocalPosition = new Vector3(0, 0, random);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = Vector3.MoveTowards(transform.localPosition, randomPosition, speed * Time.deltaTime);
-        GetComponentInChildren<Animator>().SetFloat("currentSpeed", speed);
+        if (GetComponent<Health>().IsDead()) return;
+        if (wait)
+        {
+            if (GetComponentInChildren<Animator>() != null)
+            {
+                GetComponentInChildren<Animator>().SetFloat("currentSpeed", 0);
+            }
+            return;
+        }
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, randomLocalPosition, speed * Time.deltaTime);
+        if (GetComponentInChildren<Animator>() != null)
+        {
+            GetComponentInChildren<Animator>().SetFloat("currentSpeed", speed);
+        }
+        if (Vector3.Distance(transform.localPosition, randomLocalPosition) <= 0.1f)
+        {
+            Attack();
+        }
     }
     private void Attack()
     {
-        randomPosition = new Vector3(Random.Range(0, 18), 0, 0);
+        if (GetComponentInChildren<Animator>() != null)
+        {
+            GetComponentInChildren<Animator>().SetFloat("currentSpeed", 0);
+        }
+        var lookPos = player.transform.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = rotation;
+        GetComponentInChildren<Animator>().SetTrigger("attack");
+        wait = true;
+        int random = Random.Range(0, 18);
+        randomLocalPosition = new Vector3(0, 0, random);
+        Invoke("StopWaiting", 3f);
+    }
+    private void StopWaiting()
+    {
         wait = false;
     }
 }
